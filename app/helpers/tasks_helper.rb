88895b88020e5c2ff984d619e0b53c29a4035381
Oliver Garcia <ogarci5@gmail.com>
2015-02-16 04:16:19 -0600

    Contextual classes are nice to have

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,9 @@
 module TasksHelper
+  def task_list_group_class(task)
+    if task.past_due?
+      task.completed? ? 'list-group-item-danger':'list-group-item-success'
+    else
+      ''
+    end
+  end
 end
