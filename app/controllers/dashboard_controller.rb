d01abc6d0ae0c730aacb0d5c427b919f7ace55d2
Oliver Garcia <ogarci5@gmail.com>
2015-02-16 03:36:02 -0600

    Today shouldn't include completed sorry`

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @todays_tasks = Task.today
+    @todays_tasks = Task.today.not_completed
     @this_weeks_tasks = Task.this_week
     @slack_messages = Karen::Slack::Message.today
   end
