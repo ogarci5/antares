b8b5c51b8bee3b356d84583d95f4ee4f19227cec
Oliver Garcia <ogarci5@gmail.com>
2015-02-18 22:40:06 +0000

    Current Time zone pls

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,10 @@ module Antares
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = true
+    
+# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
